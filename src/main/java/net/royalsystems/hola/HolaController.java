3069aa65ddcaef0dca3fea884e6412a60a59ae76
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-11-17 12:20:32 -0500

    Agrega ejemplo Webflux

--- src/main/java/net/royalsystems/hola/HolaController.java
+++ src/main/java/net/royalsystems/hola/HolaController.java
@@ -3,11 +3,24 @@ package net.royalsystems.hola;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RestController
 public class HolaController {
 	
     @GetMapping("/hola")
     public String hola() {
         return "Hola";
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> getMono() {
+        return Mono.just("Hola desde Mono");
+    }
+
+    @GetMapping("/flux")
+    public Flux<String> getFlux() {
+        return Flux.just("Elemento 1", "Elemento 2", "Elemento 3");
     }
 }
